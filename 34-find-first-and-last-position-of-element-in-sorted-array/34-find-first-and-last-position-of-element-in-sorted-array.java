271fcbbf591b8ff07c86d632e09d1b17ba400990
Varshini K <78270481+Varshini-k-02@users.noreply.github.com>
2022-02-14 20:14:54 +0530

    Updated optimized solution

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,3 +1,4 @@
+//Brute force TC : O(N+M)
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         int n=nums.length;
@@ -17,5 +18,43 @@ class Solution {
             return new int[]{-1,-1};
         }
         return new int[]{res[0],res[ans.size()-1]};
+    }
+}
+
+//Optimized TC : O(NlogN)
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] ans={-1,-1};
+        ans[0] = search(nums, target, true);
+        if (ans[0] != -1) {
+            ans[1] = search(nums, target, false);
+        }
+        return ans;
+        
+    }
+    int search(int[] nums,int target,boolean firstStartIndex){
+        int ans=-1; 
+         
+        int start=0;
+        int end=nums.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(target<nums[mid]){
+                end=mid-1;
+            }
+            else if(target>nums[mid]){
+                start=mid+1;
+            }
+            else{
+                ans=mid;
+                if(firstStartIndex==true){
+                    end=mid-1;
+                }
+                else{
+                    start=mid+1;
+                }
+            }
+        }
+        return ans;
     }
 }
