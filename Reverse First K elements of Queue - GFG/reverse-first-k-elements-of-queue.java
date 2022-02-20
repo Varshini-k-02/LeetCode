55fd7b84640fe7765790b5227f8513112e7a46a1
Varshini K <78270481+Varshini-k-02@users.noreply.github.com>
2022-02-20 22:19:02 +0530

    Added solution - LeetHub

---
+++ Reverse First K elements of Queue - GFG/reverse-first-k-elements-of-queue.java
@@ -0,0 +1,73 @@
+// { Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+class ModifyQueue {
+    public static void main(String[] args) {
+
+        // Taking input using class Scanner
+        Scanner sc = new Scanner(System.in);
+
+        // Taking total number of testcases
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            // Taking count of total number of elements
+            int n = sc.nextInt();
+
+            // Taking count of total elements
+            // that need to be reversed
+            int k = sc.nextInt();
+
+            // Creating a Queue
+            Queue<Integer> q = new LinkedList<>();
+
+            // adding all the elements to the Queue
+            while (n-- > 0) {
+                q.add((int)sc.nextInt());
+            }
+
+            // Creating an object of class GfG
+            GfG g = new GfG();
+
+            // calling modifyQueue of class GfG
+            // and passing Queue and k as arguments
+            // and storing the reuslt in a new Queue
+            Queue<Integer> ans = g.modifyQueue(q, k);
+
+            // Printing all the elements from the
+            // new Queue and polling them out
+            while (!ans.isEmpty()) {
+                int a = ans.peek();
+                ans.poll();
+                System.out.print(a + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class GfG {
+    // Function to reverse first k elements of a queue.
+    public Queue<Integer> modifyQueue(Queue<Integer> q, int k) {
+        // add code here.
+        int sizee=q.size();
+        Queue<Integer> q1=q;
+        Queue<Integer> q2=new LinkedList<>();
+        Stack<Integer> spare = new Stack<>();
+        for(int i=0;i<k;i++){
+            spare.push(q1.remove());
+        }
+        for(int i=0;i<k;i++){
+            q2.add(spare.pop());
+        }
+        for(int i=k;i<sizee;i++){
+            q2.add(q1.remove());
+        }
+        return q2;
+    }
+}
