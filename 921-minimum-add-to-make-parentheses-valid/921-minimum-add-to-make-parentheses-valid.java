42f5eae8cfc101b20b2eafd89c2eeb8c7bb25871
Varshini K <78270481+Varshini-k-02@users.noreply.github.com>
2022-02-21 19:49:46 +0530

    Time: 3 ms (20.41%), Space: 42.3 MB (8.32%) - LeetHub

--- 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.java
+++ 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.java
@@ -3,18 +3,13 @@ class Solution {
         Stack<Character> stack = new Stack<>();
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
-            if(ch=='('){
-                stack.push(ch);
-            }
-            else if(ch==')' && !stack.isEmpty() && stack.peek()=='(' ){
+            if(ch==')' && !stack.isEmpty() && stack.peek()=='(' ){
                 stack.pop();
             }
-            else if(ch==')'){
+            else {
                 stack.push(ch);
             }
-            else{
-                continue;
-            }
+            
         }
         return stack.size();
         
