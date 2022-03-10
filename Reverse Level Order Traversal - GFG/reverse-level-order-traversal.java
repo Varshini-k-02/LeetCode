313f145121c52ffbf1424ac3e3154196ac1c9bb7
Varshini K <78270481+Varshini-k-02@users.noreply.github.com>
2022-03-10 21:00:29 +0530

    Added solution - LeetHub

---
+++ Reverse Level Order Traversal - GFG/reverse-level-order-traversal.java
@@ -0,0 +1,162 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GFG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+    public static void main (String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t=Integer.parseInt(br.readLine());
+        while(t > 0){
+            String s = br.readLine();
+            Node root = buildTree(s);
+            Tree g = new Tree();
+            ArrayList<Integer> ans = g.reverseLevelOrder(root);
+
+            for (Integer val: ans) 
+                System.out.print(val+" "); 
+            System.out.println();
+            t--;
+        }
+    }
+}
+
+
+ 
+// } Driver Code Ends
+
+
+
+/* 
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+*/
+
+class Tree
+{
+    public ArrayList<Integer> reverseLevelOrder(Node node) 
+    {
+        ArrayList<Integer> ans=new ArrayList<>();
+        Stack<Node> S = new Stack();
+        Queue<Node> Q = new LinkedList();
+        Q.add(node);
+  
+
+        while (Q.isEmpty() == false)
+        {
+            
+            node = Q.peek();
+            Q.remove();
+            S.push(node);
+  
+            /* Enqueue right child */
+            if (node.right != null)
+                // NOTE: RIGHT CHILD IS ENQUEUED BEFORE LEFT
+                Q.add(node.right);
+                 
+            /* Enqueue left child */
+            if (node.left != null)
+                Q.add(node.left);
+        }
+  
+        // Now pop all items from stack one by one and print them
+        while (S.empty() == false)
+        {
+            node = S.peek();
+            ans.add(node.data);
+            S.pop();
+        }
+        return ans;
+    }
+}      
