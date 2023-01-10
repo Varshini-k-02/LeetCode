bd42b0281473e487cedd370bc15ea5975fa3cf04
Varshini K <78270481+Varshini-k-02@users.noreply.github.com>
2023-01-10 17:54:52 +0530

    Added solution - LeetHub

---
+++ Subset Sums - GFG/subset-sums.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            int N=sc.nextInt();
+			ArrayList<Integer> arr = new ArrayList<>();
+			for(int i = 0; i < N ; i++){
+			    arr.add(sc.nextInt());
+			}
+            Solution ob = new Solution();
+         
+            ArrayList<Integer> ans = ob.subsetSums(arr,N);
+            Collections.sort(ans);
+            for(int sum : ans){
+                System.out.print(sum+" ");
+            }
+            System.out.println();
+        }  
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java//User function Template for Java
+class Solution{
+    ArrayList<Integer> subsetSums(ArrayList<Integer> arr, int N){
+        // code here
+        ArrayList<Integer> sumSet = new ArrayList<>();
+        func(0,0,arr,N,sumSet);
+        Collections.sort(sumSet);
+        return sumSet;
+    }
+    static void func(int ind,int sum,ArrayList<Integer> arr, int N,ArrayList<Integer> sumSet){
+        if(ind==N){
+            sumSet.add(sum);
+            return;
+        }
+        func(ind+1,sum+arr.get(ind),arr,N,sumSet);
+        func(ind+1,sum,arr,N,sumSet);
+        
+    }
+}
