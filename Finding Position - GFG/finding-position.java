2327b169e7f7daa3dac2537d595db208ce563eec
Varshini K <78270481+Varshini-k-02@users.noreply.github.com>
2023-01-11 12:19:42 +0530

    Added solution - LeetHub

---
+++ Finding Position - GFG/finding-position.java
@@ -0,0 +1,29 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.util.*;
+import java.io.*;
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            long n = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+
+            System.out.println(ob.nthPosition(n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution {
+    static long nthPosition(long n){
+        // code here
+        if(n==1)
+            return 1;
+        return nthPosition(n/2)*2;
+    }
+}
